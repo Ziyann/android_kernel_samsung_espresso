80549eb9337120e3ac44465da41ea5327dc2db22
x0169938 <vasyl.yushchyshen@ti.com>
2012-11-07 16:42:14 +0200

    USB: MUSB: Fix L3 error when USB-OTG plug/unplug to PC many times during Kernel boot.
    L3 CUSTOM SRESP error appears on early stage of kernel boot and USB OTG repeating  plug/unplug  to PC.
    Source:
    [    2.258453] Master 0x0        MPU
    [    2.258453] L4CFG OPCODE  0x00000002
    
    Root cause is MUSB-OMAP2430 runtime suspending during PHY power on.
    
    Fix:
    pm_runtime_get_sync() is used.
    
    modified:   drivers/usb/musb/omap2430.c
    
    Change-Id: I59fa9db641caba811123f3724fbc17333f1458b1
    Signed-off-by: x0169938 <vasyl.yushchyshen@ti.com>

--- drivers/usb/musb/omap2430.c
+++ drivers/usb/musb/omap2430.c
@@ -392,7 +392,10 @@ static void musb_otg_notifier_work(struct work_struct *data_notifier_work)
 		if (musb->gadget_driver)
 			pm_runtime_get_sync(musb->controller);
 #endif
+		pm_runtime_get_sync(dev->parent);
 		otg_init(musb->xceiv);
+		pm_runtime_mark_last_busy(dev->parent);
+		pm_runtime_put_autosuspend(dev->parent);
 		break;
 
 	case USB_EVENT_CHARGER:
