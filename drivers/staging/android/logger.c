758075c0a4703bda112864bd2ef7053457620257
Andreas Blaesius <skate4life@gmx.de>
2015-09-22 00:45:45 +0200

    logger: reduce reserved memory for radio
    Change-Id: I57b6c62bd24afbfa44a64de07d1a0c492dcac946

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -705,7 +705,7 @@ static struct logger_log VAR = { \
 
 DEFINE_LOGGER_DEVICE(log_main, LOGGER_LOG_MAIN, 256*1024)
 DEFINE_LOGGER_DEVICE(log_events, LOGGER_LOG_EVENTS, 256*1024)
-DEFINE_LOGGER_DEVICE(log_radio, LOGGER_LOG_RADIO, 512*1024)
+DEFINE_LOGGER_DEVICE(log_radio, LOGGER_LOG_RADIO, 256*1024)
 DEFINE_LOGGER_DEVICE(log_system, LOGGER_LOG_SYSTEM, 256*1024)
 
 static struct logger_log *get_log_from_minor(int minor)
